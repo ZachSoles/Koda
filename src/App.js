adbccd9485540780ee9683fc876dbb8b286ffcee
Zachery Soles <zacherysoles@Zacherys-MacBook-Pro.local>
2020-11-27 15:43:43 -0900

    Trying to get koda to work

--- src/App.js
+++ src/App.js
@@ -1,43 +1,39 @@
-import logo from './logo.svg';
-import temp from './temp.png';
 import useImage from 'use-image';
 import './App.css';
 import { Stage, Layer, Rect, Text, Circle, Line, Image } from 'react-konva';
 
 function App() {
-  // const url = 'https://www.searchpng.com/wp-content/uploads/2018/12/Apple-iPhone-XR-672x1024.png';
-  const url = 'temp.png'
-  const [image] = useImage(url);
 
+  const handleExport = () => {
+    const uri = stageRef.current.toImage();
+    console.log(uri);
+    // we also can save uri as file
+    // but in the demo on Konva website it will not work
+    // because of iframe restrictions
+    // but feel free to use it in your apps:
+    // downloadURI(uri, 'stage.png');
+  };
+
+
+  const Mockup = () => {
+    const [image] = useImage('https://www.searchpng.com/wp-content/uploads/2018/12/Apple-iPhone-XR-672x1024.png');
+    return <Image width={400} height={600} draggable image={image} />;
+  };
+
+  const Screenshot = () => {
+    const [image] = useImage('https://raw.githubusercontent.com/ZachSoles/My-Portfolio/master/src/Resources/nyx1.png');
+    return <Image width={250} height={525} draggable image={image} />;
+  };
   return (
-    <div>
+    <div class="temp">
       <Stage width={window.innerWidth} height={window.innerHeight}>
       <Layer>
-        <Text text="Some text on canvas" fontSize={15} />
-        <Rect
-          draggable
-          x={20}
-          y={50}
-          width={100}
-          height={100}
-          fill="red"
-          shadowBlur={10}
-        />
-        <Circle x={200} y={100} radius={50} fill="green" />
-        <Line
-          x={20}
-          y={200}
-          draggable
-          points={[0, 0, 100, 0, 100, 100]}
-          tension={0.5}
-          closed
-          stroke="black"
-          fillLinearGradientStartPoint={{ x: -50, y: -50 }}
-          fillLinearGradientEndPoint={{ x: 50, y: 50 }}
-          fillLinearGradientColorStops={[0, 'red', 1, 'yellow']}
-        />
-        <Image draggable image={image} />
+        <Text draggable text="Welcome to Koda!" fontSize={25} />
       </Layer>
+      <Layer>
+          <Screenshot/>
+          <Mockup />
+        </Layer>
     </Stage>
     </div>
   );
